5acaaee71633085450851e1314642e29684a612e
hokorobi <hokorobi.hokorobi@gmail.com>
2019-02-23 20:58:41 +0900

    Change sidebar localtoc to globaltoc

--- conf.py
+++ conf.py
@@ -96,7 +96,7 @@ html_static_path = ['_static']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-html_sidebars = {'**': ['localtoc.html', 'searchbox.html']}
+html_sidebars = {'**': ['globaltoc.html', 'searchbox.html']}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
